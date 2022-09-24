961fd8374da17016d59b772bf9a0faf7edb02469
NS011618 <96785237+NS011618@users.noreply.github.com>
2022-09-24 13:17:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http=require("http");
 const fs=require("fs");
-
+const args=require("minimist")(process.argv.slice(2));
 //read file from local folder
 /*fs.readFile("home.html",(err,home)=>{
     console.log(home.toString());
@@ -62,4 +62,4 @@ http
              break;
     }
  })
- .listen(3000);
+ .listen(args["port"]);
